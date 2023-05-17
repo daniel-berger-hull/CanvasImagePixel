4733175d093dcac7586d16f7985c3dfe56d7afd0
Dan Berger <daniel.berger.hull@gmail.com>
2023-05-17 15:22:18 -0400

    fixed the color accessing and started the addressing of specific pixel based on position

--- readme.md
+++ readme.md
@@ -1,1 +1,21 @@
-dan 
+***************************************************************************************************************************
+	
+	This project is an experiment about color identification in the data of a bitmap image, and its goal is to find
+	an heuristic that can identify regions of similar color (output can be squares or rectangles)
+
+***************************************************************************************************************************
+
+May 17
+======
+   Simple page created, and an image of the US election of 2016 is loaded and displayed.
+   A button been added, that calls a method who can identify the color in the bitmap.
+
+
+ To DO:
+   First experiment would be to try to eleminate the numbers present in the image (which is the electoral count for a state)
+
+
+
+Create an inflate box
+  start with a point
+  then a larger box iteratively x,y --> x-1,y-1 to x+1,y+1 --> x+2,y+2
--- web/mycode.js
+++ web/mycode.js
@@ -1,3 +1,12 @@
+const WARM_WHITE     = 16316922;
+const DEMOCRAT_BLUE  =  15759459;
+const REPUBLICAN_RED =  6917830;
+
+const RED   = 255;
+const GREEN = 65280;
+const BLUE   = 16711680;
+
+
 const buildRgb = (imageData) => {
         const rgbValues = [];
 
@@ -27,7 +36,29 @@ const buildRgbCode = (imageData) => {
     return rgbValues;
 };
 
+const codeToRGB = (code) => {
 
+
+    // const red1 =  code >> 16;
+    // const green1 = code >> 8;
+    // const green1 = code >> 8;
+    
+    return { red:   (code >> 16) & 255,
+             green: (code >> 8) & 255,
+             blue:  code & 255
+            };
+
+}
+
+const isDark = (rgb) => {
+    if (rgb.red > 20)    return false;
+    if (rgb.green > 20)  return false;
+    if (rgb.blue > 20)   return false;
+
+    return true;
+}
+
+// This method will do a total of all the occurence of a specific color code in the image...
 
 const analyseColors = (rgbArray) => {
 
@@ -35,24 +66,32 @@ const analyseColors = (rgbArray) => {
     const colorCodeMap = new Map();
     console.log(`Total Pixels ${rgbArray.length}`);
 
-
-
     rgbArray.forEach(element => {
         
         const count = colorCodeMap.get(element);
         if (count === undefined) {
-            colorCodeMap.set(element,0);
+            colorCodeMap.set(element,1);
         } else {
             const newTotal = count+1;
             colorCodeMap.set(element,newTotal);
         }
     });
 
+
+    const totalColors = colorCodeMap.size;
+    console.log("Total number of color is " + totalColors);
     const iterator1 = colorCodeMap.entries();
     for (const nextColor of iterator1) {
 
-        if (nextColor[1] > 100)
+        if (nextColor[1] > 1000)
              console.log( nextColor[0] + "  ["  + codeToHex(nextColor[0]) + "] --> " + nextColor[1] );
+        
+         const rgb = codeToRGB(nextColor[0]);
+
+         if ( isDark(rgb) ) 
+             console.log( nextColor[0] + "  ["  + codeToHex(nextColor[0]) + "] --> " + nextColor[1] );
+
+
       }
 
 }
@@ -69,6 +108,166 @@ const codeToHex = (colorCode) => {
     
 }
 
+const indexToXY = (index, canvasWidth, canvasHeight) => {
+
+}
+
+
+const XYtoIndex = (x,y, canvasWidth, canvasHeight) => {
+    return ((canvasWidth * y) + x) * 4;
+}
+
+const alterimage = (imageData) => {
+
+    const rgbValues = [];
+
+    const total = imageData.data.length;
+    let lastMajorColor;
+
+
+    let countWhite = 0;
+    let countRedOrBlue = 0;
+    
+
+    for (let i = 0; i < imageData.data.length; i += 4) {
+        
+        const value = (imageData.data[i] << 16) + (imageData.data[i + 1] << 8) + imageData.data[i + 2];
+
+        switch(value) {
+            case WARM_WHITE:
+                    imageData.data[i]   = 255;
+                    imageData.data[i+1] = 255;
+                    imageData.data[i+2] = 255;
+                    lastMajorColor = WARM_WHITE;
+                break;
+            
+            case DEMOCRAT_BLUE:
+                    imageData.data[i] = 255;
+                    imageData.data[i+1] = 0;
+                    imageData.data[i+2] = 0;
+                    // lastMajorColor = DEMOCRAT_BLUE;
+                    lastMajorColor = BLUE;
+                break;
+
+            
+           case REPUBLICAN_RED:
+                    imageData.data[i]   = 0;
+                    imageData.data[i+1] = 0;
+                    imageData.data[i+2] = 255;
+                    // lastMajorColor = REPUBLICAN_RED;
+                    lastMajorColor = RED;
+                break;
+        
+            default:
+
+                // *** The code below is very bad, as it us IF statement into a switch case block!! Replace it as soon as you can...
+                const rgb = codeToRGB(value);
+                if ( isDark(rgb) ) {
+                    //console.log( nextColor[0] + "  ["  + codeToHex(nextColor[0]) + "] --> " + nextColor[1] );
+
+                    if (lastMajorColor === DEMOCRAT_BLUE || lastMajorColor === REPUBLICAN_RED) {
+                        countRedOrBlue++;                    
+                    } else
+                        countWhite++;
+
+    
+                    const maskingColor  = codeToRGB(lastMajorColor);
+                    imageData.data[i]   = maskingColor.red;
+                    imageData.data[i+1] = maskingColor.green;
+                    imageData.data[i+2] = maskingColor.blue;
+                
+                }
+
+        }
+     
+        
+    }
+
+    console.log("Final Count is White = " + countWhite + " Red/Blue = " + countRedOrBlue);
+
+    
+}
+
+
+
+const findContour = (imageData) => {
+
+    console.log("AlterImages 2");
+    const rgbValues = [];
+
+    const total = imageData.data.length;
+    let lastMajorColor;
+
+
+    let countWhite = 0;
+    let countRedOrBlue = 0;
+
+    const canvasWidth  = imageData.width;
+    const canvasHeight = imageData.height;
+    
+    // const indexToXY = (index, canvasWidth, canvasHeight) => {
+
+    // }
+    
+
+    for (let i = 0; i < imageData.data.length; i += 4) {
+        
+        const value = (imageData.data[i] << 16) + (imageData.data[i + 1] << 8) + imageData.data[i + 2];
+
+        switch(value) {
+            case WARM_WHITE:
+                    imageData.data[i]   = 255;
+                    imageData.data[i+1] = 255;
+                    imageData.data[i+2] = 255;
+                    lastMajorColor = WARM_WHITE;
+                break;
+            
+            case DEMOCRAT_BLUE:
+                    imageData.data[i] = 255;
+                    imageData.data[i+1] = 0;
+                    imageData.data[i+2] = 0;
+                    // lastMajorColor = DEMOCRAT_BLUE;
+                    lastMajorColor = BLUE;
+                break;
+
+            
+           case REPUBLICAN_RED:
+                    imageData.data[i]   = 0;
+                    imageData.data[i+1] = 0;
+                    imageData.data[i+2] = 255;
+                    // lastMajorColor = REPUBLICAN_RED;
+                    lastMajorColor = RED;
+                break;
+        
+            default:
+
+                // *** The code below is very bad, as it us IF statement into a switch case block!! Replace it as soon as you can...
+                const rgb = codeToRGB(value);
+                if ( isDark(rgb) ) {
+                    //console.log( nextColor[0] + "  ["  + codeToHex(nextColor[0]) + "] --> " + nextColor[1] );
+
+                    if (lastMajorColor === DEMOCRAT_BLUE || lastMajorColor === REPUBLICAN_RED) {
+                        countRedOrBlue++;                    
+                    } else
+                        countWhite++;
+
+    
+                    const maskingColor  = codeToRGB(lastMajorColor);
+                    imageData.data[i]   = maskingColor.red;
+                    imageData.data[i+1] = maskingColor.green;
+                    imageData.data[i+2] = maskingColor.blue;
+                
+                }
+
+        }
+     
+        
+    }
+
+    console.log("Final Count is White = " + countWhite + " Red/Blue = " + countRedOrBlue);
+
+    
+}
 
 
 
@@ -76,64 +275,83 @@ const codeToHex = (colorCode) => {
 const canvas = document.getElementById("my-canvas");
 const context = canvas.getContext("2d");
 const img = new Image()
-img.src = "./usa.png"
+ img.src = "./usa2.png";
+//img.src = "./test.png";
+
 img.onload = () => {
     
     console.log("getImageData");
     const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
 
-
    
-
-    // const rgbData = buildRgbCode(imageData);
-
-    // analyseColors(rgbData);
-
-    // const imgData = context.getImageData(0, 0, canvas.width, canvas.height);
-    
-     console.log("imageData.data.length is " + imageData.data.length);
-
-    // for (let i = 0; i < imageData.data.length; i += 4) {
-    //     imageData.data[i] = 255
-    // }
-
-    // context.putImageData(imageData, 0, 0)
-    // console.log(" Conversion done and putImageData called... ");
-
 
     context.drawImage(img, 0, 0);
 }
 
 
-const testButton = document.getElementById('testbutton');
+
+// alterimage
+
+const recolorButton = document.getElementById('recolorbutton');
+const findcontourButton = document.getElementById('findcontourbutton');
 
 
-testButton.addEventListener('click', () => {
-
-    //testColorChange();
-   const imgData = context.getImageData(0, 0, canvas.width, canvas.height);
-
-   for (let i = 300000; i < 300000+100; i += 4) {
-    console.log(`${i} - [${imgData.data[i]},${imgData.data[i+1]},${imgData.data[i+2]},${imgData.data[0]}]`)
-    }
+canvas.addEventListener("click", func = (e) =>{
 
 
 
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
 
-   for (let i = 0; i < imgData.data.length; i += 4) {
-     imgData.data[i] = 0
-   }
 
-   
+    const index = XYtoIndex( e.clientX ,e.clientY - canvas.offsetTop, canvas.width, canvas.height);
 
-//    13012585
-//     6519024
+    console.log("Clicked!  [" + e.clientX + "," + (e.clientY - canvas.offsetTop) + "] --> " + index);
 
-//    #698EC6 // blue  13012585    105, 142, 198
+    
 
-//    #F07863  // red   6519024  6519024    240, 120, 99
-   context.putImageData(imgData, 0, 0)
+    // imageData.data[i]   = maskingColor.red;
+    // imageData.data[i+1] = maskingColor.green;
+    // imageData.data[i+2] = maskingColor.blue;
+    // printf( "click: screen(%.1f|%.1f) client(%.1f|%.1f) click count = %d",
+    //         e.screenX, e.screenY,
+    //         ,
+    //         e.click_count );
+  });
+
+
+
+
+
+recolorButton.addEventListener('click', () => {
+
+   const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+
+    alterimage(imageData);
+    console.log("imageData.data.length is " + imageData.data.length);
+
+   context.putImageData(imageData, 0, 0)
 })
+
+
+findcontourButton.addEventListener('click', () => {
+
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+ 
+     // const rgbData = buildRgbCode(imageData);
+     // analyseColors(rgbData);
+ 
+     findContour(imageData);
+ 
+     console.log("imageData.data.length is " + imageData.data.length);
+ 
+ 
+ //    for (let i = 300000; i < 300000+100; i += 4) {
+ //     console.log(`${i} - [${imageData.data[i]},${imageData.data[i+1]},${imageData.data[i+2]},${imageData.data[0]}]`)
+ //     }
+ 
+    context.putImageData(imageData, 0, 0)
+ })
 
 
 
