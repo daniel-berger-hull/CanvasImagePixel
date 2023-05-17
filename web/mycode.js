1fa0005da5fedf89d9a3e98bed1ee1987c280015
Dan Berger <daniel.berger.hull@gmail.com>
2023-05-16 20:38:40 -0400

    Added a button, and some changes on the image can be seen when clicking on it...

--- web/mycode.js
+++ web/mycode.js
@@ -20,7 +20,7 @@ const buildRgbCode = (imageData) => {
     const total = imageData.data.length;
 
     for (let i = 0; i < imageData.data.length; i += 4) {
-
+        
         const value = (imageData.data[i] << 16) + (imageData.data[i + 1] << 8) + imageData.data[i + 2];
         rgbValues.push(value);
     }
@@ -69,20 +69,8 @@ const codeToHex = (colorCode) => {
     
 }
 
-    // const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
-    //     const hex = x.toString(16)
-    //     return hex.length === 1 ? '0' + hex : hex
-    // }).join('')
-
-  
-    // const processImage = () => {
-    //     console.log("Image loaded2!");
-
-    //     const img = document.querySelector("img");
-    //     const ctx = img.getContext("2d");
 
 
-    // }
 
     
 const canvas = document.getElementById("my-canvas");
@@ -90,12 +78,65 @@ const context = canvas.getContext("2d");
 const img = new Image()
 img.src = "./usa.png"
 img.onload = () => {
-    context.drawImage(img, 0, 0)
+    
+    console.log("getImageData");
     const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
 
-    const rgbData = buildRgbCode(imageData);
-
-    analyseColors(rgbData);
 
    
+
+    // const rgbData = buildRgbCode(imageData);
+
+    // analyseColors(rgbData);
+
+    // const imgData = context.getImageData(0, 0, canvas.width, canvas.height);
+    
+     console.log("imageData.data.length is " + imageData.data.length);
+
+    // for (let i = 0; i < imageData.data.length; i += 4) {
+    //     imageData.data[i] = 255
+    // }
+
+    // context.putImageData(imageData, 0, 0)
+    // console.log(" Conversion done and putImageData called... ");
+
+
+    context.drawImage(img, 0, 0);
+}
+
+
+const testButton = document.getElementById('testbutton');
+
+
+testButton.addEventListener('click', () => {
+
+    //testColorChange();
+   const imgData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+   for (let i = 300000; i < 300000+100; i += 4) {
+    console.log(`${i} - [${imgData.data[i]},${imgData.data[i+1]},${imgData.data[i+2]},${imgData.data[0]}]`)
+    }
+
+
+
+
+   for (let i = 0; i < imgData.data.length; i += 4) {
+     imgData.data[i] = 0
+   }
+
+   
+
+//    13012585
+//     6519024
+
+//    #698EC6 // blue  13012585    105, 142, 198
+
+//    #F07863  // red   6519024  6519024    240, 120, 99
+   context.putImageData(imgData, 0, 0)
+})
+
+
+
+const testColorChange = () => {
+    console.log("testColorChange");''
 }
